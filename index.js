87fc3b28887721fada797a373bbf9f0099fbd787
KanrinZy <kirzykrai@gmail.com>
2023-02-22 21:33:15 +0300

    1

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-function $4fa36e821943b400$var$ConvertValue(value) {
+function ConvertValue(value) {
     const vals = {
-        "Kilograms": 1000,
-        "Pounds": 453.59,
-        "Ounces": 28.35,
-        "Grams": 1,
-        "SugarCubes": 4
+        'Kilograms': 1000,
+        'Pounds': 453.59,
+        'Ounces': 28.35,
+        'Grams': 1,
+        'SugarCubes': 4,
     };
-    const converfromvalue = document.getElementById("ConvertFrom").value;
-    const valuegr = value * vals[converfromvalue];
-    const converttovalue = document.getElementById("ConvertTo").value;
-    document.getElementById("Result").innerHTML = +(valuegr / vals[converttovalue]).toFixed(2);
+
+    const valConvertFrom = document.getElementById('convert-from').value;
+    const valInGr = value * vals[valConvertFrom];
+    const valConvertT = document.getElementById('convert-To').value;
+
+    document.getElementById('result').innerHTML = +(valInGr / vals[valConvertT]).toFixed(2);
 }
-
-
-//# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ function ConvertValue(value) {
         'Pounds': 453.59,
         'Ounces': 28.35,
         'Grams': 1,
-        'SugarCubes': 4
+        'SugarCubes': 4,
     };
 
-    const converfromvalue = document.getElementById("ConvertFrom").value;
-    const valuegr = value * vals[converfromvalue];
-    const converttovalue = document.getElementById("ConvertTo").value;
+    const valConvertFrom = document.getElementById('convert-from').value;
+    const valInGr = value * vals[valConvertFrom];
+    const valConvertT = document.getElementById('convert-To').value;
 
-    document.getElementById("Result").innerHTML = +(valuegr / vals[converttovalue]).toFixed(2);
+    document.getElementById('result').innerHTML = +(valInGr / vals[valConvertT]).toFixed(2);
 }
